89b4e3c292464031f881afcddaa7e3fb08e00a1b
Mitch <Mitch@myself.com>
2022-09-09 19:02:24 +0800

    hot commit

--- src/main/java/com/atguigu/mygit/gitty.java
+++ src/main/java/com/atguigu/mygit/gitty.java
@@ -4,5 +4,6 @@ public class gitty {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 }
